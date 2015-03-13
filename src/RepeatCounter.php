d8cb76615668a0ccfedc52cd5c5dab69a1385089
Patrick <student@epicodus.com>
2015-03-13 09:59:43 -0700

    composer installed

---
+++ src/RepeatCounter.php
@@ -0,0 +1,19 @@
+<?php
+
+    class RepeatCounter {
+
+        function countRepeats($input_search, $input_string) {
+            if ($input_search == $input_string) {
+                return "1"
+            }
+
+
+            // foreach ($input_search as $search) {
+            //     foreach ($input_string as $string) {}
+            //
+            //         if (strtolower($search) == strtolower($string)) {
+            //             $final_score++;
+            // }
+
+        }
+    }
